6ab21e8ac081ae538728cf95d2ad8d839ac3ddde
dxd-future <obuhovsemen797@gmail.com>
2025-10-27 17:11:31 +0900

    uptdate 0.1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var person = new Person();
+
+person.Print();
+
+class Person
+{
+    public string Name = "Семён";
+    public int Age = 18;
+
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+    }
+}
