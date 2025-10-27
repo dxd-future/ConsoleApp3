51934b5a54f7496a0f8a73d6f0b68eff5ca3f277
dxd-future <obuhovsemen797@gmail.com>
2025-10-27 19:23:11 +0900

    add classes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,65 @@
-﻿var person = new Person();
+﻿//var person = new Person();
 
-person.Print();
+//person.Print();
 
-class Person
+//class Person
+//{
+//    public string Name = "Семён";
+//    public int Age = 18;
+
+//    public void Print()
+//    {
+//        Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+//    }
+//}
+
+//// экземпляр класса
+//Calculator Cal = new Calculator();
+//// вызов метода Sum у экземпляра Cal
+//Console.WriteLine(Cal.Sum(1,2));
+//// вызов метода vivod у экземпляра Cal
+//Cal.vivod(1, 2);
+//class Calculator
+//{
+//    // публичный метод 
+//    public int Sum(int a, int b)
+//    {
+//        return a + b;
+//    }
+//    // приватный метод
+//    private int Multiply(int a, int b)
+//    {
+//        return a * b;
+//    }
+//    // используем приватный метод в другом методе внутри класса, так как private запрещает его использование вне класса
+//    public void vivod(int a, int b)
+//    {
+//        Console.WriteLine(Multiply(a,b));
+//    }
+//}
+
+User user1 = new User("Semen", "gmail@mail.com", 18);
+user1.vivod();
+class User
 {
-    public string Name = "Семён";
-    public int Age = 18;
+    // свойства пользователя
+    private string Username { get; set; }
+    private string Email { get; set; }
+    private int Age { get; set; }
 
-    public void Print()
+    // конструктор, понадобиться для создания экземпляра
+    public User(string username, string email, int age)
     {
-        Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+        Username = username;
+        Email = email;
+        Age = age;
+    }
+
+    // метод для вывода информации о пользователе
+    public void vivod()
+    {
+        Console.WriteLine($"Имя пользователя: {Username}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Возраст: {Age}");
     }
 }
